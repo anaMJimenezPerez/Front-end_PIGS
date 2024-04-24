a2eb95cb2115e0dd63120509348b329ee3cf632d
AnaSantana016 <anasantanaojeda.16@gmail.com>
2024-04-24 19:48:13 +0100

    Creation of the home

---
+++ myApp/src/app/pages/home/home.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  products: any[] = [
+    {
+    name: 'jar', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar1', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar2', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar3', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar4', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+      },
+      {
+        name: 'jar5', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+      },
+      {
+        name: 'jar6', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+      },
+      {
+        name: 'jar7', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+      },
+      {
+        name: 'jar8', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+        },
+        {
+          name: 'jar9', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+        },
+        {
+          name: 'jar10', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+        },
+        {
+          name: 'jar11', price: '12€', urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+        },
+  ]
+}
