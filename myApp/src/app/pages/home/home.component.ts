c1328cf1bed0c605e2a640c8d632f7cd3b84d2d0
AnaSantana016 <anasantanaojeda.16@gmail.com>
2024-04-30 13:22:16 +0100

    Show More and Show Less button works correctly.

--- myApp/src/app/pages/home/home.component.ts
+++ myApp/src/app/pages/home/home.component.ts
@@ -8,42 +8,47 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   products: any[] = [
     {
-    name: 'jar', price: '12€', tags: ['cat1'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    name: 'jar', price: '12€', tags: ['cat1'], rating: 1.5, creation_time: "28/03/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
     },
     {
-      name: 'jar1', price: '12€', tags: ['cat2'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+      name: 'jar1', price: '12€', tags: ['cat2'], rating: 1.6 , creation_time: "28/04/2024",urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
     },
     {
-      name: 'jar2', price: '12€', tags: ['cat3'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+      name: 'jar2', price: '12€', tags: ['cat3'], rating: 1.8, creation_time: "28/03/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
     },
     {
-      name: 'jar3', price: '12€', tags: ['cat4'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+      name: 'jar3', price: '12€', tags: ['cat4'], rating: 2.8, creation_time: "28/04/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
     },
     {
-      name: 'jar4', price: '12€', tags: ['cat1','cat2'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
-      },
-      {
-        name: 'jar5', price: '12€', tags: ['cat1','cat3'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
-      },
-      {
-        name: 'jar6', price: '12€', tags: ['cat1','cat4'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
-      },
-      {
-        name: 'jar7', price: '12€', tags: ['cat2','cat3'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
-      },
-      {
-        name: 'jar8', price: '12€', tags: ['cat2','cat4'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
-        },
-        {
-          name: 'jar9', price: '12€', tags: ['cat1','cat2'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
-        },
-        {
-          name: 'jar10', price: '12€', tags: ['cat1','cat2'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
-        },
-        {
-          name: 'jar11', price: '12€', tags: ['cat1','cat2'] ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
-        },
+    name: 'jar4', price: '12€', tags: ['cat1','cat2'], rating: 5.8, creation_time: "28/03/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar5', price: '12€', tags: ['cat1','cat3'], rating: 6.8, creation_time: "28/04/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar6', price: '12€', tags: ['cat1','cat4'], rating: 1.8, creation_time: "28/04/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar7', price: '12€', tags: ['cat2','cat3'], rating: 7.8, creation_time: "28/03/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+    name: 'jar8', price: '12€', tags: ['cat2','cat4'], rating: 8.8, creation_time: "28/04/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar9', price: '12€', tags: ['cat1','cat2'], rating: 4.8, creation_time: "28/04/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar10', price: '12€', tags: ['cat1','cat2'],rating: 5.8, creation_time: "28/04/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
+    {
+      name: 'jar11', price: '12€', tags: ['cat1','cat2'], rating: 1.8, creation_time: "28/04/2024" ,urlImage: 'https://s1.eestatic.com/2020/08/05/imprescindibles/branded_content-marcas_destacadas-amazon_510709568_157068665_1706x960.jpg'
+    },
   ]
+
+  currentDate = new Date();
+  formattedCurrentDate = `${this.currentDate.getDate()}/${this.currentDate.getMonth() + 1}/${this.currentDate.getFullYear()}`;
+
+  productsFavorite = this.products.filter((products) => products.rating > 5.00);
 
   colors: string[] = [];
   filteredProducts: any[] = [];
@@ -51,6 +56,8 @@ export class HomeComponent {
   constructor() {
     this.products.forEach(() => this.colors.push('transparent'));
     this.filteredProducts = this.products;
+    console.log(this.currentDate)
+    console.log(this.formattedCurrentDate)
   }
 
   changeColor(index: number) {
@@ -79,31 +86,41 @@ export class HomeComponent {
     }
   }
 
-  /*
-  totalRowsToShow: number = 4;
-  showAllRows: boolean = false;
 
-  showMore() {
-    this.showAllRows = !this.showAllRows;
-    this.totalRowsToShow = this.showAllRows ? this.filteredProducts.length : 4;
-  }*/
-
+  totalRowsToShowFavorite: number = 4;
   totalRowsToShow: number = 4;
   rowsPerPage: number = 4;
+  showAllRowsFavorite: boolean = false;
   showAllRows: boolean = false;
 
-  showMore() {
-    if (this.showAllRows) {
-      this.totalRowsToShow = 4;
-      this.showAllRows = false;
-    } else {
-      this.totalRowsToShow += this.rowsPerPage;
-      if (this.totalRowsToShow >= this.filteredProducts.length) {
-        this.totalRowsToShow = this.filteredProducts.length;
-        this.showAllRows = true;
-      } else {
+  showMore(id: number, list: any[], totalRowsToShow: number, showAllRows: boolean) {
+    if (id === 1) {
+      if (showAllRows) {
+        this.totalRowsToShow = 4;
         this.showAllRows = false;
+      } else {
+        this.totalRowsToShow += this.rowsPerPage;
+        if (this.totalRowsToShow >= list.length) {
+          this.totalRowsToShow = list.length;
+          this.showAllRows = true;
+        } else {
+          this.showAllRows = false;
+        }
+      }
+    } else if (id === 2) {
+      if (showAllRows) {
+        this.totalRowsToShowFavorite = 4;
+        this.showAllRowsFavorite = false;
+      } else {
+        this.totalRowsToShowFavorite += this.rowsPerPage;
+        if (this.totalRowsToShowFavorite >= list.length) {
+          this.totalRowsToShowFavorite = list.length;
+          this.showAllRowsFavorite = true;
+        } else {
+          this.showAllRowsFavorite = false;
+        }
       }
     }
   }
+
 }
