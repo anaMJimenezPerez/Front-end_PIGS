d43ee98d004df54b0682f91177f37fdc958947d6
AnaSantana016 <anasantanaojeda.16@gmail.com>
2024-05-08 23:12:56 +0100

    Add new interface

---
+++ myApp/src/app/interfaces/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: number;
+  creation_time: string;
+  address: number;
+  last_viewed: number;
+  favorites: string;
+  image: string;
+}
