c97fea0a9d212d38cc77467afe7a29d2bd3a0730
AlejandroOrtegaG <alejandroog99@gmail.com>
2024-05-09 19:22:53 +0100

    get for products in home

--- myApp/src/app/pages/home/home.component.ts
+++ myApp/src/app/pages/home/home.component.ts
@@ -58,13 +58,12 @@ export class HomeComponent implements OnInit{
         const images = productImagesMap.get(product.id) || [];
         return { ...product, images_path: images };
       });
-
       //New products
       const fifteenDaysAgo = new Date();
       fifteenDaysAgo.setDate(fifteenDaysAgo.getDate() - 15);
 
       this.newProducts = this.products.filter(product => {
-        const [day, month, year] = product.creation_time.split('/');
+        const [year, month, day] = product.creation_time.split('T')[0].split('-');
         const creationTime = new Date(Number(year), Number(month) - 1, Number(day));
         return creationTime >= fifteenDaysAgo;
       });
