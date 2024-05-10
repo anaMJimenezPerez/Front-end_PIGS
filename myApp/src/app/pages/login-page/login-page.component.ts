144ef0c23205f59311f39252522ff7fc895ccf25
anaMJimenezPerez <ana.jimenez121@alu.ulpgc.es>
2024-05-10 08:41:19 +0100

    remember me login funcionalitty implemented

--- myApp/src/app/pages/login-page/login-page.component.ts
+++ myApp/src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthUserService } from 'src/app/services/auth-user.service';
 
@@ -7,11 +7,22 @@ import { AuthUserService } from 'src/app/services/auth-user.service';
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit{
   email: string = '';
   password: string = '';
+  rememberMe: boolean = false;
   
   constructor(private router: Router, private authService: AuthUserService) {}
+
+  ngOnInit(): void {
+    const rememberedEmail = localStorage.getItem('rememberedEmail');
+    const rememberedPassword = localStorage.getItem('rememberedPassword');
+    if (rememberedEmail && rememberedPassword) {
+      this.email = rememberedEmail;
+      this.password = rememberedPassword;
+      this.rememberMe = true;
+    }
+  }
 
   navigateToSignUpPage() {
     this.router.navigateByUrl('/signupPage');
@@ -22,23 +33,29 @@ export class LoginPageComponent {
   }
 
 
-  onSubmit() {
+  onSubmit(): void {
     this.authService.login(this.email, this.password).subscribe({
       next: isAuthenticated => {
         if (isAuthenticated) {
+          if (this.rememberMe) {
+            localStorage.setItem('rememberedEmail', this.email);
+            localStorage.setItem('rememberedPassword', this.password);
+          } else {
+            localStorage.removeItem('rememberedEmail');
+            localStorage.removeItem('rememberedPassword');
+          }
           this.navigateToHomePage();
-          this.authService.login(this.email, this.password);
         } else {
           window.alert('Nombre de usuario o contraseña incorrectos');
         }
       },
       error: error => {
         let errorMessage = 'Error al iniciar sesión';
-      if (error && error.message) {
-        errorMessage += ': ' + error.message;
-      }
-      window.alert(errorMessage);
-      console.error(errorMessage, error);
+        if (error && error.message) {
+          errorMessage += ': ' + error.message;
+        }
+        window.alert(errorMessage);
+        console.error(errorMessage, error);
       }
     });
   }
