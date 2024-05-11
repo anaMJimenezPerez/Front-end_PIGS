709de35ed1e658faeecf905c87976fc03a60c6a6
ElenaaNavarroo <maria.navarro140@alu.ulpgc.es>
2024-05-11 13:30:31 +0100

    Merge with my branch

--- myApp/src/app/pages/profile/profile.component.ts
+++ myApp/src/app/pages/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { forkJoin } from 'rxjs';
 import { AuthUserService } from 'src/app/services/auth-user.service';
 import { ProductService } from 'src/app/services/product.service';
 import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 interface Product {
   id: number;
@@ -69,10 +70,9 @@ export class ProfileComponent implements OnInit {
   productImages: any[] = [];
 
   constructor(
-    private purchaseService: PurchaseService,
+    /*private purchaseService: PurchaseService,*/
     private authService: AuthUserService,
     private productService: ProductService,
-
     private userService: UserService,
   ) { }
 
@@ -82,7 +82,6 @@ export class ProfileComponent implements OnInit {
 
 
     /*forkJoin([
-
       this.purchaseService.getAllPurchaseDetails(),
       this.purchaseService.getAllPurchaseHistory(),
       this.productService.getAllProducts(),
@@ -122,7 +121,20 @@ export class ProfileComponent implements OnInit {
                 });
 
             console.log(this.details);
-            
+
+            /*
+            this.details = this.purchaseDetails.filter(detail => detail.purchase_id === loggedInUserId);
+            console.log(`Detalles de compra para la compra con ID ${loggedInUserId}:`, this.details);
+
+            this.details.forEach(detail => {
+              products = this.products.find(product => product.id === detail.product_id);
+              console.log(`Información del producto con ID ${detail.product_id}:`, products);
+
+              users = this.users.find(user => user.id === products.seller_id);
+              console.log(`Información sobre el seller con ID ${products.seller_id}:`, users);
+
+            });
+
           });
         }
       });
