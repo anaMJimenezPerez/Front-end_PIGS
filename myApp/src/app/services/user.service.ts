d922af12ee2fe379b8befd3d7b372b96411b45b5
anaMJimenezPerez <ana.jimenez121@alu.ulpgc.es>
2024-05-12 20:25:21 +0100

    direction form connected to back

--- myApp/src/app/services/user.service.ts
+++ myApp/src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { User } from 'src/app/interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,16 @@ export class UserService {
   }
   getAllUserImages(): Observable<any> {
     return this.http.get<any>('../.../../assets/data/user_images.json');
+  }
+
+  getUserInfo(email: any): Observable<any>{
+    return this.http.get<any>(`http://localhost:8080/users/getUsers?filterField=email&filterValue=${email}`);
+  }
+
+  userDirectionInfo(id: any, user: any): Observable<any>{
+    let idLong = Number(id);
+    console.log("user service: " + user.id + " " + user.address);
+    return this.http.put<User>(`http://localhost:8080/users/updateUser?id=${idLong}`, user);
   }
 
   userCartIsEmpty(userEmail: string): Observable<boolean> {
